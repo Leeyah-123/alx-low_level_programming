c54698b46e3a0f258ec28e03fa70dc9dfd4cf85b
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-10-18 17:38:52 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	char string[9] = "_putchar";
+	char word[9] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(text[i]);
+		_putchar(word[i]);
 	}
 	_putchar('\n');
 
