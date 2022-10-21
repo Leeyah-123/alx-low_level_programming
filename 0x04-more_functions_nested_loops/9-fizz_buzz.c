edb54e2b95aae953244a85915a3c515a1b5ba500
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-10-20 20:06:04 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,50 +1,36 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * fizz_buzz - Prints 1 to 100
+ * main - Prints 1 to 100
  *
- * Return: void
+ * Return: Always 0
  */
 
-void fizz_buzz(void)
+int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			printf(" FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
+			printf(" Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			printf(" Buzz");
 		}
 		else
 		{
-			if (i >= 10)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			printf(" %d", n);
 		}
-		if (i != 100)
-			_putchar(' ');	
 	}
+	printf("\n");
+	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -21,6 +21,4 @@ void print_diagonal(int n);
 
 void print_square(int size);
 
-void fizz_buzz(void);
-
 #endif
