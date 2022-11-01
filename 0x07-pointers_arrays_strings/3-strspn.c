7e98a7c3fe98f61a9c245147622dc23d504bc642
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-10-31 19:35:02 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String
+ * @accept: Substring
+ *
+ * Return: Integer
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int length = 0;
+	int flag = 0;
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == 32)
+			break;
+		for (; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				{
+					flag = 1;
+					length++;
+				}
+		}
+		if (flag == 0)
+			break;
+		else
+			flag = 0;
+	}
+	return (length);
+}
