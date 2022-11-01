f7f5e4fbd40e4bb6b361d974933d7b5310b6c120
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-10-31 19:52:39 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: String to be searched
+ * @accept: Bytes to be searched for
+ *
+ * Return: First occurence of any byte from accept in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j, flag;
+
+	flag = 0;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				flag = 1;
+				break;
+			}
+		}
+	}
+
+	if (flag == 0)
+		return ('\0');
+	return (i);
+}
