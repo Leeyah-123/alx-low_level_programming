311f2cb65eefc99a26d3284402c7f8b88b9aedfd
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-10-31 18:47:13 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _memcpy - Copies memory area
+ * @dest: Destination
+ * @src: Source
+ * @n: Amount of bytes to be copied
+ *
+ * Return: Pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	int src_len = 0;
+
+	while ((*(src + src_len)) != '\0')
+		src_len++;
+
+	for (i = 0; i < src_len; i++)
+	{
+		if (i > n)
+			break;
+		*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
