13f23f2f36e602da2784185ce633ccafe538ab0a
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-10-31 19:11:49 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i, s_len = 0, flag = 0;
+	unsigned int i;
+	int flag = 0;
 
-	while ((*(s + s_len)) != '\0')
-		s_len++;
-
-	for (i = 0; i < s_len; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if ((*(s + i)) == c)
+		if (*(s + i) == c)
 		{
 			return (s + i);
 			flag = 1;
