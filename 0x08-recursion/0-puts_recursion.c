584d1ca0f2024da694d934ed0bea3bb6ba82c483
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-02 07:28:12 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	if (*(s + 1) == '\0')
+	if (*s == 32 || *s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
