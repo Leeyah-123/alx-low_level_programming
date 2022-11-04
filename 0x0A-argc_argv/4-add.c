1903a7d2247d3e119424244c09f2363ed8456e13
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-04 04:51:18 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+/**
+ * main - Prints the name of a programme followed by a new line
+ * @argc: Number of arguments passed to program
+ * @argv: Arguments passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0 && argv[i] != '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+        return (0);
+}
+
