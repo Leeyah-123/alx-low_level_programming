283b5af27ed592ebb050ed7d4b644aa8893e513c
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-04 04:29:55 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of a programme followed by a new line
+ * @argc: Number of arguments passed to program
+ * @argv: Arguments passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf"Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(agrv[1]) * atoi(argv[2]));
+	return (0);
+}
+
