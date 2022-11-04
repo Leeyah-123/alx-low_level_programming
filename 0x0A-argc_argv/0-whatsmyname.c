103703add30e52a963ea7bba99f0bd0750bebaeb
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-04 04:04:48 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of a programme followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
