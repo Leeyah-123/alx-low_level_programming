8aa0be09a7ca16c662f079462044b2cde2df0967
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-04 14:21:56 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - Locates a character in a string
+ * @s: Pointer to string
+ * @c: Character to be found
+ *
+ * Return: Pointer to first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to stdout
+ * @str: A string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
