f3317d321c5ab4f6dba25cbbb1536d352d48d6be
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-10 14:32:13 -0800

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,37 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
+	unsigned int s1_len, s2_len, p_len, i;
 
-	p = malloc(sizeof(char) * s1_len * (s2_len );
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+		;
+
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+		;
+
+	if (n > s2_len)
+		n = s2_len;
+
+	p_len = s1_len + n;
+	p = malloc(p_len + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < p_len; i++)
+	{
+		if (i < s1_len)
+			p[i] = s1[i];
+		else
+			p[i] = s2[i - s1_len];
+	}
+
+	p[i] = '\0';
+	return (p);
 }
