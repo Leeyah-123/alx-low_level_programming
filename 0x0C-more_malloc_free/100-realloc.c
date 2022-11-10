77b40e36565e2049e6af0bf0b65394a3180855b0
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-10 15:20:26 -0800

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (i = 0; i < old_size; i++)
-		*(p + i) == *(ptr + i);
+		*(p + i) = *(ptr + i);
 
 	free(ptr);
 	return (p);
