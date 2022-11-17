3b2665e23b8412796f09686a640246d2adbfe89a
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-17 13:47:53 -0800

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,69 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - Prints a character
+ * @args: Variadic argument list
+ *
+ * Return: void
+ */
+
+void print_char(va_list args)
+{
+	char arg;
+
+	arg = va_arg(args, int);
+	printf("%c", arg);
+}
+
+/**
+ * print_int - Prints an integer
+ * @args: Variadic argument list
+ *
+ * Return: void
+ */
+
+void print_int(va_list args)
+{
+	int arg;
+
+	arg = va_arg(args, int);
+	printf("%d", arg);
+}
+
+/**
+ * print_float - Prints a float
+ * @args: Variadic argument list
+ *
+ * Return: void
+ */
+
+void print_float(va_list args)
+{
+	float arg;
+
+	arg = va_arg(args, double);
+	printf("%f", arg);
+}
+
+/**
+ * print_string - Prints a string
+ * @args: Variadic argument list
+ *
+ * Return: void
+ */
+
+void print_string(va_list args)
+{
+	char *string;
+
+	string = va_arg(args, char *);
+	if (string == NULL)
+		printf("(nil)");
+	else
+		printf("%s", string);
+}
+
+/**
  * print_all - Function that prints anything
  * @format: Types of arguments passed to the function
  *
@@ -14,13 +77,31 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0, j = 0;
 
+	types t[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
+	int flag;
+
 	va_start(args, format);
 	while (format[i])
 	{
+		j = 0;
+		flag = 0;
 		while (j < 4)
 		{
+			if (format[i] == t[j].type)
+			{
+				t[j].func(args);
+				flag = 1;
+			}
 			j++;
 		}
+
+		if (format[i + 1] && flag == 1)
+			printf(", ");
 		i++;
 	}
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -8,5 +10,19 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+/**
+ * struct structure - Declares the corresponding function to print an argument
+ * @type: Type of argument
+ * @func: Corresponding function
+ *
+ * Description: Used for printing arguments
+ */
+
+typedef struct structure
+{
+	char type;
+	void (*func)(va_list);
+} types;
 
 #endif
