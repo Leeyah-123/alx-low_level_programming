29223002b9f760638148a753f4992cd020f5f75a
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-17 01:52:59 -0800

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Prints strings, followed by a new line
+ * @separator: String to be printed between strings
+ * @n: Number of strings to be printed
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(strings, char *));
+
+		if (i + 1 == n)
+		{
+			printf("\n");
+			continue;
+		}
+
+		if (seperator != NULL)
+			printf("%s", seperator);
+	}
+
+	va_end(strings);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Function that prints anything
+ * @format: Types of arguments passed to the function
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+}
