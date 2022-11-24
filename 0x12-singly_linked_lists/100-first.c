6b209c380cf477d2809ff32b32cdcd7ed058587a
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-24 12:50:33 -0800

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - Prints a string before the main function is executed
+ *
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
