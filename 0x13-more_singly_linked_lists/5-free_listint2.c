bc014273ee0bd371aae8017071ffdd204496cfd5
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-11-30 03:16:30 -0800

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,10 +12,13 @@ void free_listint2(listint_t **head)
 	listint_t *temp1, *temp2;
 
 	temp1 = *head;
-	while ((temp2 = temp1) != NULL)
+	if (*head != NULL)
 	{
-		temp1 = temp1->next;
-		free(temp2);
+		while ((temp2 = temp1) != NULL)
+		{
+			temp1 = temp1->next;
+			free(temp2);
+		}
 	}
 
 	*head = NULL;
